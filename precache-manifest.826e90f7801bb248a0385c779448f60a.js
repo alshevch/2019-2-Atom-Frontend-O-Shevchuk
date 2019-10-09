c3d0333ad70ec8de5ac2180d0ef4b8b4ea39fa30
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-09 19:17:17 +0000

    Deploy alshevch/2019-2-Atom-Frontend-O-Shevchuk to github.com/alshevch/2019-2-Atom-Frontend-O-Shevchuk.git:gh-pages

---
+++ precache-manifest.826e90f7801bb248a0385c779448f60a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302d3f272014ce8b76e61eb4139581a4",
+    "url": "/atom-mail-2019/index.html"
+  },
+  {
+    "revision": "7bf665181bb4bd4e9a0e",
+    "url": "/atom-mail-2019/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "ce6c6e0c3d0e6da89e5c",
+    "url": "/atom-mail-2019/static/js/2.a29d3861.chunk.js"
+  },
+  {
+    "revision": "7bf665181bb4bd4e9a0e",
+    "url": "/atom-mail-2019/static/js/main.ebfe544a.chunk.js"
+  },
+  {
+    "revision": "f1d9e2f8a69fbdae66cd",
+    "url": "/atom-mail-2019/static/js/runtime-main.4e7b64ae.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/atom-mail-2019/static/media/logo.a55f1c43.svg"
+  }
+]);
